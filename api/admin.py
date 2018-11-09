414eb2b525684df4c5a840ac13784eb2edad5c85
lehoangdung0412 <lehoangdung.hcmus@gmail.com>
2018-11-10 02:22:47 +0700

    add list_display into admin to render models in default admin django

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Login)
+class Login(admin.ModelAdmin):
+    list_display = ['id', 'username', 'customer']
+
+
+@admin.register(models.Customers)
+class Customers(admin.ModelAdmin):
+    list_display = ['id', 'forename', 'surname', 'add1', 'add2', 'add3', 'postcode', 'email', 'registered']
+
+
+@admin.register(models.Orders)
+class Orders(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'registered', 'delivery_add', 'payment_type', 'date', 'status', 'session',
+                    'total']
+
+
+@admin.register(models.DeliveryAdds)
+class DeliveryAdds(admin.ModelAdmin):
+    list_display = ['id', 'forename', 'surname', 'add1', 'add2', 'add3', 'postcode', 'phone', 'email']
+
+
+@admin.register(models.OrderItems)
+class OrderItems(admin.ModelAdmin):
+    list_display = ['id', 'order', 'product', 'quantity']
+
+
+@admin.register(models.Products)
+class Products(admin.ModelAdmin):
+    list_display = ['id', 'cat', 'name', 'description', 'image', 'price']
+
+
+@admin.register(models.Categories)
+class Categories(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'image']
+
