284f98b32b4065ed1b943c37e58583398ebb49de
lehoangdung0412 <lehoangdung.hcmus@gmail.com>
2018-11-10 11:04:44 +0700

    add permission into views to permiss for CustomerViewSet, ProductViewSet, OrderViewSet and CategoryViewSet

--- api/views.py
+++ api/views.py
@@ -1,16 +1,24 @@
 from .serializers import *
 from . import models
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = models.Customers.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = models.Products.objects.all()
     serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if (self.action == 'list') | (self.action == 'retrieve'):
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAdminUser]
+        return [permission() for permission in permission_classes]
 
 
 class DeliveryAddViewSet(viewsets.ModelViewSet):
@@ -22,10 +30,24 @@ class OrderViewSet(viewsets.ModelViewSet):
     queryset = models.Orders.objects.all()
     serializer_class = OrderSerializer
 
+    def get_permissions(self):
+        if self.action == 'retrieve':
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAdminUser]
+        return [permission() for permission in permission_classes]
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = models.Categories.objects.all()
     serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        if (self.action == 'list') | (self.action == 'retrieve'):
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAdminUser]
+        return [permission() for permission in permission_classes]
 
 
 class OrderItemViewSet(viewsets.ModelViewSet):
