bd84666d433b886e2b558f27e9fba44d24ee6c7a
lehoangdung0412 <lehoangdung.hcmus@gmail.com>
2018-11-10 09:49:05 +0700

    add queryset and serializer_class of models into views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from .serializers import *
+from . import models
+from rest_framework import viewsets
 
-# Create your views here.
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = models.Customers.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = models.Products.objects.all()
+    serializer_class = ProductSerializer
+
+
+class DeliveryAddViewSet(viewsets.ModelViewSet):
+    queryset = models.DeliveryAdds.objects.all()
+    serializer_class = DeliveryAddSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = models.Orders.objects.all()
+    serializer_class = OrderSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = models.Categories.objects.all()
+    serializer_class = CategorySerializer
+
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = models.OrderItems.objects.all()
+    serializer_class = OrderItemSerializer
+
+
+class LoginViewSet(viewsets.ModelViewSet):
+    queryset = models.Login.objects.all()
+    serializer_class = LoginSerializer
