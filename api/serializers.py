85a5546fe69d5321d709494487175d29479b5b85
lehoangdung0412 <lehoangdung.hcmus@gmail.com>
2018-11-10 10:47:11 +0700

    add nested relationships for class OrderSerializer and CategorySerializer to display serializer relations

--- api/serializers.py
+++ api/serializers.py
@@ -27,6 +27,7 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True, read_only=True)
 
     class Meta:
         model = models.Orders
@@ -41,7 +42,15 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
 
     class Meta:
         model = models.Categories
         fields = '__all__'
+
+    # def create(self, validated_data):
+    #     tracks_data = validated_data.pop('products')
+    #     cat = models.Categories.objects.create(**validated_data)
+    #     for track_data in tracks_data:
+    #         models.Products.objects.create(cat=cat, **track_data)
+    #     return cat
