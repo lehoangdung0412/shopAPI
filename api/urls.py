e05cba9fe06dca4da518ec7390a9a37b991ed519
lehoangdung0412 <lehoangdung.hcmus@gmail.com>
2018-11-10 11:36:20 +0700

    add JWT into shop API

--- api/urls.py
+++ api/urls.py
@@ -1,20 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import views
+from rest_framework_jwt.views import obtain_jwt_token
 
 
 router = routers.SimpleRouter(trailing_slash=False)
-router.register('customers', views.CustomerViewSet)
-router.register('products', views.ProductViewSet)
+router.register('customers/', views.CustomerViewSet)
+router.register('products/', views.ProductViewSet)
 # router.register('delivery', views.DeliveryAddViewSet)
-router.register('orders', views.OrderViewSet)
-router.register('order-items', views.OrderItemViewSet)
-router.register('categories', views.CategoryViewSet)
+router.register('orders/', views.OrderViewSet)
+router.register('order-items/', views.OrderItemViewSet)
+router.register('categories/', views.CategoryViewSet)
 # router.register('login', views.LoginViewSet)
-router.register('current-user/orders', views.OrdersByCurrentUserViewSet)
-router.register('current-user', views.CurrentUserViewSet)
+router.register('current-user/orders/', views.OrdersByCurrentUserViewSet)
+router.register('current-user/', views.CurrentUserViewSet)
 
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
+    path('api/v1/sessions/', obtain_jwt_token, name='create-token'),
 ]
